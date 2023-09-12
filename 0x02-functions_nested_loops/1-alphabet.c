0c8dcc32c9b263130bb25fbb5d8c883ef1cf6ac3
jerrypundit <jerry.okhomina@gmail.com>
2023-09-12 19:23:13 +0000

    second commit with corrections on 1-alphabet.c && 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,11 @@
 #include "main.h"
 
 /**
- * main - entry point of the program
- * Description: This is the entry point of the program
- * Return: 0 on success
- */
-
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Check description
- * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Description: It prints the alphabet in lowercase followed by a new line
  * Return: Nothing.
  */
+
 void print_alphabet(void)
 {
 	char i;
