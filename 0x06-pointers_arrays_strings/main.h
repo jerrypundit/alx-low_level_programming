9cb63648b3bb708e80acc35eb63c59c4a5519c73
jerrypundit <jerry.okhomina@gmail.com>
2023-09-22 02:59:54 +0000

    first commit 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+int isChar(char c, int *list);
+void changer(char *ch);
+char *leet(char *);
+void print_number(int n);
+int _putchar(char c);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
